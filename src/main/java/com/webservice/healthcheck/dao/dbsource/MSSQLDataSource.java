890263d0e2d6b95e8081b0fec231687e09f2a6ee
sunil gupta <sunil.gupta@impetus.co.in>
2016-08-08 11:52:28 +0530

    Added one bean for sql server database

---
+++ src/main/java/com/webservice/healthcheck/dao/dbsource/MSSQLDataSource.java
@@ -0,0 +1,35 @@
+package com.webservice.healthcheck.dao.dbsource;
+
+import org.apache.commons.dbcp.BasicDataSource;
+
+public class MSSQLDataSource extends BasicDataSource {
+
+  public MSSQLDataSource(String username, String password, String driver,
+      String url) {
+    setUsername(username);
+    setPassword(password);
+    setDriverClassName(driver);
+    setUrl(url);
+  }
+
+  @Override
+  public synchronized void setUsername(String username) {
+    super.setUsername(username);
+  }
+
+  @Override
+  public synchronized void setPassword(String password) {
+    super.setPassword(password);
+  }
+
+  @Override
+  public synchronized void setDriverClassName(String driverClassName) {
+    super.setDriverClassName(driverClassName);
+  }
+
+  @Override
+  public synchronized void setUrl(String url) {
+    super.setUrl(url);
+  }
+}
+
