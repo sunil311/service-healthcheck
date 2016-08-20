6f222e508907a5c72cd3e8deb96625cb544f6586
kdmalviyan <kdmalviyan@gmail.com>
2016-08-21 00:36:25 +0530

    PDF report added

--- src/main/java/com/webservice/healthcheck/reports/PDFReport.java
+++ src/main/java/com/webservice/healthcheck/reports/PDFReport.java
@@ -1,18 +1,51 @@
 package com.webservice.healthcheck.reports;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.stereotype.Service;
 
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
 import com.webservice.healthcheck.model.WebServiceHistory;
 import com.webservice.healthcheck.provider.ResourceLocator;
+import com.webservice.helper.ESBHelper;
 
 @Service
 public class PDFReport implements ReportPublisher {
 	@Override
 	public void publish(Map<Integer, List<WebServiceHistory>> servicesMap,
 			ResourceLocator resourceLocator) {
+		try {
+			createPdf(resourceLocator.getTempReport() + "/ServiceReport.pdf",
+					servicesMap);
+		} catch (DocumentException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
+	/**
+	 * Creates a PDF document.
+	 * 
+	 * @param filename
+	 *            the path to the new PDF document
+	 * @param servicesMap
+	 * @throws DocumentException
+	 * @throws IOException
+	 */
+	public void createPdf(String filename,
+			Map<Integer, List<WebServiceHistory>> servicesMap)
+			throws DocumentException, IOException {
+		Document document = new Document();
+		PdfWriter.getInstance(document, new FileOutputStream(filename));
+		document.open();
+		document.add(new Paragraph(ESBHelper.createReport(servicesMap)));
+		document.close();
 	}
 }
--- src/main/java/com/webservice/helper/ESBHelper.java
+++ src/main/java/com/webservice/helper/ESBHelper.java
@@ -10,13 +10,6 @@ import java.io.UnsupportedEncodingException;
 import java.net.SocketTimeoutException;
 import java.util.List;
 import java.util.Map;
-import java.io.FileOutputStream;
-import java.io.IOException;
-
-import com.itextpdf.text.Document;
-import com.itextpdf.text.DocumentException;
-import com.itextpdf.text.Paragraph;
-import com.itextpdf.text.pdf.PdfWriter;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
@@ -160,36 +153,9 @@ public class ESBHelper {
 		return xml2String;
 	}
 
-	public static File prepareDocxReportFile() {
-		File file = new File("");
-		return file;
-	}
-
 	public static String createReport(
 			Map<Integer, List<WebServiceHistory>> servicesMap) {
 		return servicesMap.toString();
-	}
-
-	/**
-	 * Creates a PDF document.
-	 * 
-	 * @param filename
-	 *            the path to the new PDF document
-	 * @throws DocumentException
-	 * @throws IOException
-	 */
-	public void createPdf(String filename) throws DocumentException,
-			IOException {
-		// step 1
-		Document document = new Document();
-		// step 2
-		PdfWriter.getInstance(document, new FileOutputStream(filename));
-		// step 3
-		document.open();
-		// step 4
-		document.add(new Paragraph("Hello World!"));
-		// step 5
-		document.close();
 	}
 
 	public static ReportsType createReportType(String reportFormat) {
